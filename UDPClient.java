882409c802f0409f24ec62e2c272dcb6d494fbbf
João Silva <jpsouzadasilva@gmail.com>
2024-09-30 04:54:50 -0300

    att

--- UDPClient.java
+++ UDPClient.java
@@ -1,30 +1,46 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.*;
+import java.net.*;
 
 public class UDPClient {
   private static final String SERVER_ADDRESS = "127.0.0.1";
-  private static final int SERVER_PORT = 12346;
+  private static final int SERVER_PORT = 12345;
 
   public static void main(String[] args) {
-    try (DatagramSocket socket = new DatagramSocket()) {
+    try (DatagramSocket socket = new DatagramSocket();
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+
       InetAddress serverAddress = InetAddress.getByName(SERVER_ADDRESS);
 
-      while (true) {
-        System.out.print("Digite a mensagem: ");
-        byte[] messageBytes = System.console().readLine().getBytes();
+      System.out.print("Para entrar no servidor UDP, digite seu username para começar: ");
+      String username = console.readLine();
+      sendMessage(socket, username, serverAddress);
 
-        DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, serverAddress, SERVER_PORT);
-        socket.send(packet);
-
+      new Thread(() -> {
         byte[] buffer = new byte[1024];
-        DatagramPacket responsePacket = new DatagramPacket(buffer, buffer.length);
-        socket.receive(responsePacket);
-        String response = new String(responsePacket.getData(), 0, responsePacket.getLength());
-        System.out.println("Resposta do servidor: " + response);
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        while (true) {
+          try {
+            socket.receive(packet);
+            String serverMessage = new String(packet.getData(), 0, packet.getLength());
+            System.out.println(serverMessage);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }).start();
+
+      String clientMessage;
+      while ((clientMessage = console.readLine()) != null) {
+        sendMessage(socket, clientMessage, serverAddress);
       }
-    } catch (Exception e) {
+    } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private static void sendMessage(DatagramSocket socket, String message, InetAddress serverAddress) throws IOException {
+    byte[] buffer = message.getBytes();
+    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, SERVER_PORT);
+    socket.send(packet);
   }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -1,31 +1,158 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UDPServer {
-  private static final int SERVER_PORT = 12346;
+  private static final int SERVER_PORT = 12345;
+  private static Map<String, InetSocketAddress> users = new HashMap<>();
+  private static DatagramSocket socket;
 
   public static void main(String[] args) {
-    try (DatagramSocket socket = new DatagramSocket(SERVER_PORT)) {
+    try {
+      socket = new DatagramSocket(SERVER_PORT);
       System.out.println("Servidor UDP iniciado na porta " + SERVER_PORT);
 
-      byte[] buffer = new byte[1024];
-      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
       while (true) {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         socket.receive(packet);
-        String message = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Mensagem recebida de " + packet.getAddress() + ": " + message);
-
-        String response = "Mensagem recebida: " + message;
-        DatagramPacket responsePacket = new DatagramPacket(
-            response.getBytes(),
-            response.length(),
-            packet.getAddress(),
-            packet.getPort());
-        socket.send(responsePacket);
+        new ClientHandler(packet).start();
       }
-    } catch (Exception e) {
+    } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  private static class ClientHandler extends Thread {
+    private DatagramPacket packet;
+    private String username;
+    private InetSocketAddress clientAddress;
+
+    public ClientHandler(DatagramPacket packet) {
+      this.packet = packet;
+      this.clientAddress = new InetSocketAddress(packet.getAddress(), packet.getPort());
+    }
+
+    public void run() {
+      try {
+          String message = new String(packet.getData(), 0, packet.getLength());
+
+          if (users.containsValue(clientAddress)) {
+              for (Map.Entry<String, InetSocketAddress> entry : users.entrySet()) {
+                  if (entry.getValue().equals(clientAddress)) {
+                      username = entry.getKey();
+                  }
+              }
+              processClientMessage(message);
+          } else {
+              username = message;
+              synchronized (users) {
+                  users.put(username, clientAddress);
+              }
+              sendMessage("Registrado como: " + username, clientAddress);
+              sendMessage("Bem vindo ao servidor UDP", clientAddress);
+              sendMessage("Para ver todos os comandos do servidor, digite: /HELP", clientAddress);
+              System.out.println(username + " conectado.");
+          }
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+
+    private void processClientMessage(String message) throws IOException {
+      boolean messageCommand = message.startsWith("/MSG");
+      boolean fileCommand = message.startsWith("/FILE");
+      boolean helpCommand = message.equals("/HELP");
+      boolean listUsersCommand = message.equals("/LIST");
+
+      if (messageCommand) {
+        String[] parts = message.split(" ", 3);
+        if (parts.length == 3) {
+          String userToSend = parts[1];
+          String msg = parts[2];
+          sendMessage(userToSend, msg);
+        }
+      }
+
+      if (fileCommand) {
+        String[] parts = message.split(" ", 3);
+        if (parts.length == 3) {
+          String userToSend = parts[1];
+          String fileName = parts[2];
+          sendFile(userToSend, fileName);
+        }
+      }
+
+      if (helpCommand) {
+        sendMessage("Comandos disponíveis: /HELP, /LIST, /MSG <username> <mensagem>, /FILE <username> <arquivo>",clientAddress);
+      }
+
+      if (listUsersCommand) {
+        listAllUsers();
+      }
+    }
+
+    private void sendMessage(String userToSend, String msg) throws IOException {
+      InetSocketAddress target = users.get(userToSend);
+
+      boolean userOnline = target != null;
+
+      if (userOnline) {
+        sendMessage(username + ": " + msg, target);
+        sendMessage("Mensagem enviada com sucesso", clientAddress);
+      } else {
+        sendMessage("O usuário " + userToSend + " não está online.", clientAddress);
+      }
+    }
+
+    private void sendFile(String userToSend, String fileName) throws IOException {
+      InetSocketAddress target = users.get(userToSend);
+
+      if (target != null) {
+          sendMessage("Preparando para receber o arquivo: " + fileName, clientAddress);
+
+          File receivedFile = new File(System.currentTimeMillis() + fileName);
+          try (FileOutputStream fos = new FileOutputStream(receivedFile)) {
+              byte[] buffer = new byte[1024];
+              DatagramPacket filePacket = new DatagramPacket(buffer, buffer.length);
+
+              while (true) {
+                  socket.receive(filePacket);
+                  int bytesRead = filePacket.getLength();
+
+                  boolean isLastPackage = bytesRead < buffer.length;
+                  if (isLastPackage) {
+                      fos.write(filePacket.getData(), 0, bytesRead);
+                      break; 
+                  }
+                  fos.write(filePacket.getData(), 0, bytesRead);
+              }
+
+              sendMessage("Arquivo " + fileName + " recebido com sucesso de " + username, target);
+          } catch (IOException e) {
+              sendMessage("Erro ao receber o arquivo " + fileName, clientAddress);
+              e.printStackTrace();
+          }
+      } else {
+          sendMessage("O usuário " + userToSend + " não está online.", clientAddress);
+      }
+  }
+
+    private void listAllUsers() throws IOException {
+      synchronized (users) {
+        StringBuilder userList = new StringBuilder("Usuários conectados:");
+        for (String user : users.keySet()) {
+          userList.append("\n- ").append(user);
+        }
+        sendMessage(userList.toString(), clientAddress);
+      }
+    }
+
+    private void sendMessage(String message, InetSocketAddress target) throws IOException {
+      byte[] buffer = message.getBytes();
+      DatagramPacket packet = new DatagramPacket(buffer, buffer.length, target.getAddress(), target.getPort());
+      socket.send(packet);
     }
   }
 }
